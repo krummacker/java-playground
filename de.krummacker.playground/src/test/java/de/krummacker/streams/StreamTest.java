8442f82b1fa75e36d78fa8c81d0987b15435146d
Dirk Krummacker <krummacker@icloud.com>
2018-07-10 20:41:38 +0200

    Add test method for mapping return values from elements

--- de.krummacker.playground/src/test/java/de/krummacker/streams/StreamTest.java
+++ de.krummacker.playground/src/test/java/de/krummacker/streams/StreamTest.java
@@ -3,8 +3,7 @@ package de.krummacker.streams;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -39,6 +38,22 @@ public class StreamTest {
                 .map(n -> n * 2);
         List<Integer> produced = stream.collect(Collectors.toList());
         List<Integer> expected = Arrays.asList(0, 2, 4, 6, 8, 10, 12, 14, 16, 18);
+        Assertions.assertEquals(expected, produced);
+    }
+
+    @Test
+    public void testGetItemFromMapElement() throws Exception {
+
+        String[] codes = {"EUR", "CZK", "JPY"};
+        List<Map<String, String>> listOfMaps = new ArrayList();
+        for (String code : codes) {
+            Map<String, String> element = new HashMap();
+            element.put("code", code);
+            listOfMaps.add(element);
+        }
+
+        Set<String> produced = listOfMaps.stream().map(element -> element.get("code")).collect(Collectors.toSet());
+        Set<String> expected = new HashSet<>(Arrays.asList(codes));
         Assertions.assertEquals(expected, produced);
     }
 }
