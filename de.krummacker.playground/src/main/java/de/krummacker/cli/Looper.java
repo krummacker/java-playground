9106d18a378f172e9dcb01cc021c79f2c006f0dc
Dirk Krummacker <krummacker@icloud.com>
2023-02-25 21:53:40 +0100

    Add changes of past years

---
+++ de.krummacker.playground/src/main/java/de/krummacker/cli/Looper.java
@@ -0,0 +1,11 @@
+package de.krummacker.cli;
+
+public class Looper {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        for (int i = 0; i < 10; ++i) {
+            System.out.println(i);
+        }
+    }
+}
